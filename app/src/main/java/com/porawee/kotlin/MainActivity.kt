a5dabde234a3e08febf15e0aaf2988f0d619885d
porawee <58160123@go.buu.ac.th>
2018-05-10 09:59:08 +0700

    android login

--- app/src/main/java/com/porawee/kotlin/MainActivity.kt
+++ app/src/main/java/com/porawee/kotlin/MainActivity.kt
@@ -3,16 +3,32 @@ package com.porawee.kotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val username = "Porawee"
+    val password = "1234"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        var EditText = findViewById<EditText>(R.id.textUser)
-        textUser.text
+//        val EditText = findViewById<EditText>(R.id.textUser)
+        textUser.text.toString()
+        textPass.text.toString()
+        btnLogin.setOnClickListener {
+           var check = checkLogin()
+            Toast.makeText(this,""+check,Toast.LENGTH_LONG).show()
+        }
 
+    }
+
+    private fun checkLogin() : Boolean{
+        if (username == textUser.text.toString() && password == textPass.text.toString()){
+            return true
+        }
+            return false
     }
 }
