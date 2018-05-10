b5917fceb30e86dc3e6a2e33d52c4b4ffeefe70e
porawee <58160123@go.buu.ac.th>
2018-05-10 09:35:21 +0700

    android

--- app/src/main/java/com/porawee/kotlin/MainActivity.kt
+++ app/src/main/java/com/porawee/kotlin/MainActivity.kt
@@ -2,11 +2,17 @@ package com.porawee.kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        var EditText = findViewById<EditText>(R.id.textUser)
+        textUser.text
+
     }
 }
